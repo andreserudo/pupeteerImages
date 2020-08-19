cbf955b5ee4f46db6f6a680502f3e4e64af66ad7
Andre Serudo <andreserudo@gmail.com>
2020-08-18 22:40:29 -0400

    collected src from fishes imgs

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto('https://animalcrossing.fandom.com/wiki/Fish_(New_Horizons)');
 
+  const imgList = await page.evaluate( () => {
+    
+    const nodeList = document.querySelectorAll('.roundy.sortable.jquery-tablesorter tr a img');
+
+    const imgArray = [ ...nodeList ];
+
+    const imgList = imgArray.map( ({ dataset }) => ({
+      src: dataset.src
+      }));
+
+    return imgList
+
+  });
+
+  fs.writeFile(
+    'fishesImg.json',
+    JSON.stringify(imgList, null, 2),
+    err => {
+      if(err) throw new Error('Something went wrong');
+    }
+  )
+  
   await browser.close();
+
+  console.log('Job has ended with ' + imgList.length + ' fishes images received.');
 })();
+
